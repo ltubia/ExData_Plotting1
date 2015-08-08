2d6aed6a4858f0715489c7b8195af800a6a9df38
ltubia <leandro.tubia@gmail.com>
2015-08-08 19:47:00 +0000

    Version 2

--- plot4.R
+++ plot4.R
@@ -34,7 +34,7 @@ plot4<-function() {
         plot(y$DateTime, y$Sub_metering_1, type = "l", main = "", ylab ="Global Active Power", xlab = "")
         points(y$DateTime, y$Sub_metering_2, type = "l", col = "red")
         points(y$DateTime, y$Sub_metering_3, type = "l", col = "blue")
-        legend("topright", pch = "----", col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), bty="n")
+        legend("topright", lty = c(1,1,1), col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"), bty="n")
         # Draw graph: bottom right
         plot(y$DateTime, y$Global_reactive_power, type = "l", main = "", ylab ="Global_reactive_power", xlab = "datetime")
         
